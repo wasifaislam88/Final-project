54cb96e525bf512e78f5af9d00f3687fa5b2ef78
wasifaislam88 <wasifaislam88@gmail.com>
2025-05-17 22:54:32 +0600

    added a second commit

--- src/component/ToggleSection.jsx
+++ src/component/ToggleSection.jsx
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const toggleSection = () => {
+
+    const[state,setState] = useState('available')
+    const handleState = (newState) =>{
+        setState(newState)
+    }
+
     return (
-        <div className='flex flex-row justify-between w-11/12 mx-auto'>
+        <div className='flex flex-row justify-between w-11/12 mx-auto mt-5'>
             {/* left section */}
             <section>
-            left
+           {
+            state == 'available' ? "Available Players" : "Selected Players"
+           }
             </section>
 
             {/* right section */}
 
-            <section>
-                right
+            <section className='border rounded-xl'>
+
+                <button 
+
+                style={{
+                    color:"blue",
+                    fontWeight:700,
+                    borderRadius:"20px",
+                    backgroundColor:state == 'available'? "#FFFF00":"white",
+                    // color:"red"
+                }}
+                
+                onClick={()=> handleState('available')}
+
+                 className='pr-2 py-2'>Available</button>
+
+                <button
+                
+                style={{
+                    color:"blue",
+                    fontWeight:700,
+                    borderRadius:"20px",
+                    backgroundColor:state == 'available'? "white":"#FFFF00",
+                    // color:"red"
+                }}
+                
+                 onClick={()=> handleState('Selected')}
+
+                className='pl-2 py-2'>Selected</button>
+
             </section>
 
 
